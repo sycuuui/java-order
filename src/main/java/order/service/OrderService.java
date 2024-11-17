80d44ed1c30e68c60089dc8c1396c34921696d66
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-11-17 21:29:46 +0900

    feat: 주문 가격에 따른 배달비 측정 구현

--- src/main/java/order/service/OrderService.java
+++ src/main/java/order/service/OrderService.java
@@ -1,5 +1,26 @@
 package order.service;
 
-public class OrderService {
+import order.model.Orders;
 
+public class OrderService {
+    private Orders orders;
+
+    public OrderService(Orders orders) {
+        this.orders = orders;
+    }
+
+    public int deliverPrice() {
+        int totalOrderPrice = orders.totalOrderPrice();
+        if (totalOrderPrice < 50000) {
+            return 2000;
+        }
+        if (totalOrderPrice < 100000) {
+            return 1000;
+        }
+        return 0;
+    }
+
+    public int totalPrice() {
+        return deliverPrice() + orders.totalOrderPrice();
+    }
 }
